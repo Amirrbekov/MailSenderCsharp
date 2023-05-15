67ab46e594c7f7db17402470f0b2d0de55a15995
Amirrbekov <amirbekovaleh@gmail.com>
2023-05-15 19:58:03 +0400

    first commit

---
+++ MailSenderCsharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+
+//hotmail
+
+var smtpClient = new SmtpClient("smtp.office365.com")
+{
+    Port = 587,
+    Credentials = new NetworkCredential("YOUR MAIL ADRESS", "PASSWORD"),
+    EnableSsl = true,
+};
+
+smtpClient.Send("YOUR MAIL ADRESS", "OTHER MAIL", "SUBJECT MESSAGE", "BODY MESSAGE");
+
+
+// gmail
+
+var smtpClient1 = new SmtpClient("smtp.gmail.com")
+{
+    Port = 587,
+    Credentials = new NetworkCredential("YOUR MAIL ADRESS", "PASSWORD"),
+    EnableSsl = true,
+};
+
+smtpClient1.Send("YOUR MAIL ADRESS", "OTHER MAIL", "SUBJECT MESSAGE", "BODY MESSAGE");
